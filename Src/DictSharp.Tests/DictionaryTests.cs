47594306183d1959ea931eabc7ee2234cf4f9ddf
ajlopez <webmaster@ajlopez.com>
2014-03-02 16:57:47 -0300

    New test with internal refactor, WIP

--- Src/DictSharp.Tests/DictionaryTests.cs
+++ Src/DictSharp.Tests/DictionaryTests.cs
@@ -1,9 +1,9 @@
 ﻿namespace DictSharp.Tests
 {
     using System;
-    using System.Text;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
@@ -35,12 +35,80 @@
         }
 
         [TestMethod]
+        public void SetAndGetTwoItems()
+        {
+            Dictionary<int> dictionary = new Dictionary<int>();
+
+            dictionary.SetItem("alpha", 1);
+            dictionary.SetItem("beta", 2);
+            Assert.AreEqual(1, dictionary.GetItem("alpha"));
+            Assert.AreEqual(2, dictionary.GetItem("beta"));
+        }
+
+        [TestMethod]
+        public void SetInInverseOrderAndGetTwoItems()
+        {
+            Dictionary<int> dictionary = new Dictionary<int>();
+
+            dictionary.SetItem("beta", 2);
+            dictionary.SetItem("alpha", 1);
+            Assert.AreEqual(1, dictionary.GetItem("alpha"));
+            Assert.AreEqual(2, dictionary.GetItem("beta"));
+        }
+
+        [TestMethod]
+        public void SetResetAndGetItem()
+        {
+            Dictionary<int> dictionary = new Dictionary<int>();
+
+            dictionary.SetItem("one", 1);
+            dictionary.SetItem("one", 2);
+            Assert.AreEqual(2, dictionary.GetItem("one"));
+        }
+
+        [TestMethod]
         public void SetItemAndGetUnknownItem()
         {
             Dictionary<int> dictionary = new Dictionary<int>();
 
             dictionary.SetItem("one", 1);
             Assert.AreEqual(0, dictionary.GetItem("two"));
+        }
+
+        [TestMethod]
+        public void SetAndGetTenItems()
+        {
+            Dictionary<int> dictionary = new Dictionary<int>();
+
+            for (var k = 1; k <= 10; k++)
+                dictionary.SetItem(string.Format("key{0:00}", k), k);
+
+            for (var k = 1; k <= 10; k++)
+                Assert.AreEqual(k, dictionary.GetItem(string.Format("key{0:00}", k)));
+        }
+
+        [TestMethod]
+        public void SetInInverseOrderAndGetTenItems()
+        {
+            Dictionary<int> dictionary = new Dictionary<int>();
+
+            for (var k = 10; k >= 1; k--)
+                dictionary.SetItem(string.Format("key{0:00}", k), k);
+
+            for (var k = 1; k <= 10; k++)
+                Assert.AreEqual(k, dictionary.GetItem(string.Format("key{0:00}", k)));
+        }
+
+        [TestMethod]
+        public void SetAndGetTwentyItems()
+        {
+            Dictionary<int> dictionary = new Dictionary<int>();
+
+            for (var k = 1; k <= 20; k++)
+                dictionary.SetItem(string.Format("key{0:00}", k), k);
+
+            for (var k = 1; k <= 20; k++)
+                Assert.AreEqual(k, dictionary.GetItem(string.Format("key{0:00}", k)));
         }
     }
 }
--- Src/DictSharp/TreeLeafNode.cs
+++ Src/DictSharp/TreeLeafNode.cs
@@ -20,17 +20,64 @@
 
         public T GetItem(string key)
         {
-            for (int k = 0; k < this.nkeys; k++)
-                if (this.keys[k] == key)
-                    return this.values[k];
+            int position = this.GetPosition(key);
+
+            if (position >= 0)
+                return this.values[position];
 
             return default(T);
         }
 
         public void SetItem(string key, T value)
         {
-            this.values[this.nkeys] = value;
-            this.keys[this.nkeys++] = key;
+            int position = this.GetPosition(key);
+
+            if (position >= 0)
+            {
+                this.values[position] = value;
+            }
+            else
+            {
+                int newposition = -1 - position;
+
+                if (newposition >= this.nkeys)
+                {
+                    this.values[this.nkeys] = value;
+                    this.keys[this.nkeys++] = key;
+                }
+                else
+                {
+                    for (int k = this.nkeys; k > newposition; k--)
+                    {
+                        this.values[k] = this.values[k - 1];
+                        this.keys[k] = this.keys[k - 1];
+                    }
+
+                    this.values[newposition] = value;
+                    this.keys[newposition] = key;
+                    this.nkeys++;
+                }
+            }
+        }
+
+        private int GetPosition(string key)
+        {
+            int k;
+
+            for (k = 0; k < this.nkeys; k++)
+            {
+                int compare = this.keys[k].CompareTo(key);
+
+                if (compare < 0)
+                    continue;
+
+                if (compare == 0)
+                    return k;
+
+                break;
+            }
+
+            return -k - 1;
         }
     }
 }
